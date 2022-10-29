78dc00a363356007441c68f76da7a19a3c8c8e26
Rohit Tokala <rtokala@purdue.edu>
2022-10-29 15:00:04 -0400

    Fix OAuth issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,30 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <main>Loading...</main>
+  }
 
   return (
     <main>
       <h1>Guestbook</h1>
+      {session ? (
+        <div>
+          <p>
+            hi {session.user?.name}
+          </p>
 
-      <button onClick={() => signIn("discord")}>Login with Discord</button>
-      <button onClick={() => signIn("google")}>Login with Google</button>
+          <button onClick={() => signOut()}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => signIn("discord")}>Login with Discord</button>
+          <button onClick={() => signIn("google")}>Login with Google</button>
+        </div>
+      )}
+
     </main>
   );
 };
